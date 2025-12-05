cedae17e6c1914a2871ea0cb403ec93ed19e302a
Gabriel-Muerte <rochalucasvb@gmail.com>
2025-12-05 09:23:04 -0300

    SETIMO

--- provaFINAL/API/Program.cs
+++ provaFINAL/API/Program.cs
@@ -1,4 +1,18 @@
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDataContext>();
+
+builder.Services.AddCors(options =>
+    options.AddPolicy("Acesso Total",
+        configs => configs
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod())
+);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
