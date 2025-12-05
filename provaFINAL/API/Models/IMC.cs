e0b3da666ec69f4e7840e98ab93559622b57e5cf
Gabriel-Muerte <rochalucasvb@gmail.com>
2025-12-05 09:37:07 -0300

    OITAVO

--- provaFINAL/API/Models/IMC.cs
+++ provaFINAL/API/Models/IMC.cs
@@ -12,7 +12,7 @@ public class IMC
     //Propriedade/Atributo/Característica
     public string id { get; set; }
     public string nome { get; set; } = string.Empty;
-    public string descricao { get; set; } = string.Empty;
+    public string classificacao { get; set; } = string.Empty;
     public double altura { get; set; }
     public double peso { get; set; }
     public double imc { get; set; }
--- provaFINAL/API/Program.cs
+++ provaFINAL/API/Program.cs
@@ -15,6 +15,27 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<IMC> imcs = new List<IMC>
+{
+    
+};
+
+app.MapGet("/", () => "IMC");
+
+//Cadastrar produto
+app.MapPost("/api/imc/cadastrar", ([FromBody] IMC imc,
+    [FromServices] AppDataContext ctx) =>
+{
+    IMC? resultado =
+        ctx.imcs.FirstOrDefault(x => x.id == imc.id);
+    if (resultado is not null)
+    {
+        return Results.Conflict("IMC já existente!");
+    }
+    imc.imc = imc.peso / (imc.altura * imc.altura);
+    ctx.imcs.Add(imc);
+    ctx.SaveChanges();
+    return Results.Created("", imc);
+});
 
 app.Run();
